9d3ebd16df8cc052273ecd43f27fce2233193875
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-07-28 12:57:25 +0530

    Task 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-
-import './App.css';
-import Task1 from './Task1';
-import Task2 from './Task2';
-import Task3 from './Task3';
+import { MyProvider } from "./Context";
+import "./App.css";
+import Task1 from "./Task1";
+import Task2 from "./Task2";
+import Task3 from "./Task3";
+import Task4 from "./Task4";
 
 function App() {
   return (
     <div className="App">
-     <Task1 />
-     <hr />
-     <Task2 />
-     <hr />
-     <Task3 />
-    <hr />
+      <Task1 />
+      <hr />
+      <Task2 />
+      <hr />
+      <Task3 />
+      <hr />
+      <MyProvider>
+        <Task4 />
+      </MyProvider>
+      <hr />
     </div>
   );
 }
