01ae827952e569dcc849739ac84d22ac9580a8d8
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-07-28 12:25:38 +0530

    Task3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Task1 from './Task1';
 import Task2 from './Task2';
+import Task3 from './Task3';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
      <Task1 />
      <hr />
      <Task2 />
+     <hr />
+     <Task3 />
+    <hr />
     </div>
   );
 }
