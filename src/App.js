d470741ca15a123e2c3db66b2ef3d940a56971d0
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-07-28 11:19:00 +0530

    Task2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Task1 from './Task1';
+import Task2 from './Task2';
 
 function App() {
   return (
     <div className="App">
      <Task1 />
+     <hr />
+     <Task2 />
     </div>
   );
 }
