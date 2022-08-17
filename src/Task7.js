dd1cfa3a48e41a41e07715afba82ac49140af312
IshaAgrawal09 <ishaagrawal@cedcoss.com>
2022-08-17 15:29:39 +0530

    Task7 (b)

--- src/Task7.js
+++ src/Task7.js
@@ -1,21 +1,31 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 
 const Task7 = () => {
-  const [val, setVal] = useState("");
-
+  const [val1, setVal1] = useState("");
+  const [val2, setVal2] = useState("");
   const [color, setColor] = useState("black");
   const [bgColor, setBgColor] = useState("white");
 
-  const val1 = useMemo(() => {
-    if (val !== "") {
-      return parseInt(val) + 1;
+  //   USEMEMO FUNCTION
+  //   const val1 = useMemo(() => {
+  //     if (val !== "") {
+  //       return parseInt(val) + 1;
+  //     }
+  //   }, []);
+  //   const val2 = useMemo(() => {
+  //     if (val !== "") {
+  //       return parseInt(val) + 2;
+  //     }
+  //   }, []);
+
+  //   CALLBACK FUNCTION
+  const handle = useCallback(() => {
+    let show = document.getElementById("num").value;
+    if (show !== "") {
+      setVal1(parseInt(show) + 1);
+      setVal2(parseInt(show) + 2);
     }
-  });
-  const val2 = useMemo(() => {
-    if (val !== "") {
-      return parseInt(val) + 2;
-    }
-  });
+  }, []);
 
   //   CHANGE THEME
   const theme = () => {
@@ -34,8 +44,9 @@ const Task7 = () => {
       <input
         type="number"
         id="num"
-        value={val}
-        onChange={(event) => setVal(event.target.value)}
+        onChange={handle}
+        // value={val}
+        // onChange={(event) => setVal(event.target.value)}
       />
 
       <div style={{ color: color, backgroundColor: bgColor }}>
